3c01ec1db705633a54accd4926a315b179d06bcf
Mitchell Kotler <mitch@mitchellkotler.net>
2022-12-20 13:48:40 -0500

    soft timeout

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+Test soft time out patterns
 """
+
+import time
 
 from documentcloud.addon import AddOn
 
+ADDON_ID = 242
 
-class HelloWorld(AddOn):
+
+class TestTimeout(AddOn):
     """An example Add-On for DocumentCloud."""
+
+    soft_time_limit = 10
+    addon_id = ADDON_ID
+
+    def __init__(self):
+        super().__init__()
+        self._start = time.time()
+        self._document_generator = None
+
+    def rerun_addon(self, documents):
+        """Re-run the add on with the same parameters with the remaining documents"""
+        self.client.post(
+            "addon_runs/",
+            json={
+                "addon": self.addon_id,
+                "parameters": self.data,
+                "documents": [d.id for d in documents],
+            },
+        )
+
+    def get_documents(self):
+        """Get documents from either selected or queried documents"""
+
+        if self.documents:
+            documents = self.client.documents.list(id__in=self.documents)
+        elif self.query:
+            documents = self.client.documents.search(self.query)
+
+        for document in documents:
+            if self.soft_timeout():
+                self.rerun_addon(documents)
+                self.set_message(
+                    "Soft time out, continuing rest of documents in a new run"
+                )
+                break
+            yield document
+
+    def soft_timeout(self):
+        return time.time() - self._start > self.soft_time_limit
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        print(f"Processing {self.get_document_count()} documents...")
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            print(f"Processing document {document.title}...")
+            self.set_message(f"processing document {document.title}...")
+            time.sleep(5)
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    TestTimeout().main()
