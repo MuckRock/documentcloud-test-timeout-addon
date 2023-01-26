a93eddec6ad24e8ba0db84b584e2e107c48b4fb4
Mitchell Kotler <mitch@mitchellkotler.net>
2023-01-26 15:50:27 -0500

    print

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ class TestTimeout(SoftTimeOutAddOn):
     def main(self):
         """The main add-on functionality goes here."""
         self.set_message(f"Processing {self.get_document_count()} documents...")
+        print(f"Processing {self.get_document_count()} documents...")
         for document in self.get_documents():
             self.set_message(f"Processing document {document.title}...")
+            print(f"Processing document {document.title}...")
             time.sleep(5)
 
 
